221993fdcb453f0df48435a63c6f766ea31f8130
pizone2 <rlawlstj0123@naver.com>
2023-04-20 11:02:30 +0900

    commit

--- src/main/java/com/iu/base/home/HomeController.java
+++ src/main/java/com/iu/base/home/HomeController.java
@@ -1,13 +1,48 @@
 package com.iu.base.home;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.iu.base.aoptest.Card;
+import com.iu.base.aoptest.Transport;
+import com.iu.base.board.BoardFileVO;
+import com.iu.base.util.Pager;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private Transport transport;
+	
+	@Autowired
+	private Card card;
+	
 	@GetMapping("/")
 	public String home() {
 		return "home";
+	}
+	
+	@GetMapping("/use")
+	public void use()throws Exception {
+		Pager pager = new Pager();
+		pager.setKind("Bus Title");
+		transport.useBus(pager);
+		
+		BoardFileVO boardFileVO = new BoardFileVO();
+		boardFileVO.setFileName("Subway File");
+		
+		transport.useSubWay(boardFileVO);
+		 
+		transport.useBus(pager);
+		 
+		
+		 
+		transport.useSubWay(boardFileVO);
+		 
+		
+		 
+		transport.takeWalk();
+		 
 	}
 }
